89f8465b1dc1177118caba86626ee22ffc8c38e6
Lee Ye Rim <yelimi0416@gmail.com>
2018-10-15 13:31:54 +0900

    nmt source

---
+++ nmt_project/data.js
@@ -0,0 +1,33 @@
+var filterObj = {
+  "type" : ["korea","china","japan","snackbar","cafe","meat","sashimi","pasta","buffet","rice","noodle","fastfood","tv"],
+  "region" :["muk","sinnae","junghwa","sangbong","mangwoo","meonmok"]
+};
+
+if(document.getElementById("selectFilter").value==null)
+{
+  alert("앱");
+}
+/*
+맛집이름
+음식종류
+한식, 중식, 일식, 분식,카페, 고기, 회/스시, 파스타, 뷔페, 밥, 면, 패스트푸드 ,뷔페, tv맛집
+
+지역
+묵동, 신내동, 중화동, 상봉동, 망우동, 면목동
+
+가격
+만원미만, 1만원대, 2만원대, 3만원 이상
+
+맛
+매콤, 달달, 새콤, 짭쪼름, 담백, 건강
+
+분위기
+조용한, 복잡한, 고급스런, 깔끔한, 허름한, 유명한
+
+방문목적
+혼자방문, 데이트, 가족외식, 여자끼리, 모임 ,다이어트
+
+배달여부
+
+
+*/
